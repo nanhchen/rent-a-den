569846ef720edd0a19f1838f60f26a75c838a41a
Mailyn <ngu.mailyn.yen@gmail.com>
2025-09-13 15:44:02 -0700

    sign in mobile

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,26 @@
+import { FaUser } from "react-icons/fa6";
+import { RiLockPasswordFill } from "react-icons/ri";
+
 const Login = () => {
   return (
-    <div>
-      <p className="font-bold text-xl">SIGN IN</p>
-      <input placeholder="Username" className="underline" />
-      <input placeholder="Password" className="underline" />
-      <p>
-        Don't have an account? <span>Register here</span>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="font-bold text-xl mb-[8%]">SIGN IN</p>
+      <div className="flex items-center gap-2 border-b-1 mb-[6%] py-1">
+        <FaUser className="opacity-50" />
+        <input placeholder="Username" className="focus:outline-none" />
+      </div>
+      <div className="flex items-center gap-2 border-b-1 mb-[10%] py-1">
+        <RiLockPasswordFill className="opacity-50" />
+        <input placeholder="Password" className="focus:outline-none" />
+      </div>
+
+      <p className="mb-[10%]">
+        Don't have an account?{" "}
+        <span className="ml-4 font-bold text-rad-blue-100">Register here</span>
       </p>
+      <button className="px-[12%] py-[2%] rounded-sm bg-rad-blue-100 text-white font-bold">
+        LOG IN
+      </button>
     </div>
   );
 };
